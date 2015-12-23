47db63ccc45664e8025ecb2489f11b87b215891f
hejianjun1991 <myhelang@126.com>
2015-12-23 19:25:49 +0800

    xxx

--- choume/src/main/java/com/shichai/www/choume/activity/BaseActivity.java
+++ choume/src/main/java/com/shichai/www/choume/activity/BaseActivity.java
@@ -52,35 +52,5 @@ public class BaseActivity extends AppCompatActivity {
         }
     }
 
-    protected void setActionBarLayout(){
-        android.app.ActionBar actionBar = getActionBar();
-        if(actionBar != null){
-            actionBar.setDisplayShowHomeEnabled( false );
-            actionBar.setDisplayShowCustomEnabled(true);
-
-            LayoutInflater inflator = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View v = inflator.inflate(R.layout.actionbar_layout, null);
-            android.app.ActionBar.LayoutParams layout = new android.app.ActionBar.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
-            actionBar.setCustomView(v,layout);
-        }else {
-
-        }
-    }
-
-    protected void transparent(){
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-            window.setNavigationBarColor(Color.TRANSPARENT);
-        }
-    }
-
 
 }
