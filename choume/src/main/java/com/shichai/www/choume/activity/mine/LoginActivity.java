ab73ee300c090db7e5e844dfb3babda851b82d68
hejianjun1991 <myhelang@126.com>
2015-12-24 20:07:56 +0800

    xxx

--- choume/src/main/java/com/shichai/www/choume/activity/mine/LoginActivity.java
+++ choume/src/main/java/com/shichai/www/choume/activity/mine/LoginActivity.java
@@ -31,12 +31,6 @@ public class LoginActivity extends ActionBarActivity implements View.OnClickList
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
