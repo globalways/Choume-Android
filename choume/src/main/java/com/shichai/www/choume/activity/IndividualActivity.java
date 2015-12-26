038b9778c851ea5973f0fa5fec8ae2bc69d6f791
wang-mac <onebyte@qq.com>
2015-12-26 16:03:21 +0800

    个人信息界面调整

--- choume/src/main/java/com/shichai/www/choume/activity/IndividualActivity.java
+++ choume/src/main/java/com/shichai/www/choume/activity/IndividualActivity.java
@@ -6,8 +6,11 @@ import android.view.MenuItem;
 
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import com.globalways.choume.proto.nano.OutsouringCrowdfunding.CfUser;
 import com.outsouring.crowdfunding.R;
 import com.shichai.www.choume.activity.mine.profile.CertApplyActivity;
+import com.shichai.www.choume.application.MyApplication;
 
 /**
  * Created by HeJianjun on 2015/12/22.
@@ -15,6 +18,7 @@ import com.shichai.www.choume.activity.mine.profile.CertApplyActivity;
 public class IndividualActivity extends BaseActivity implements View.OnClickListener {
 
     private RelativeLayout rlToCert;
+    private TextView tvNick, tvTel, tvSex, tvCert, tvAddr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,6 @@ public class IndividualActivity extends BaseActivity implements View.OnClickList
         initActionBar();
         setTitle("个人设置");
         initViews();
-
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -37,6 +40,11 @@ public class IndividualActivity extends BaseActivity implements View.OnClickList
     private void initViews() {
         rlToCert = (RelativeLayout) findViewById(R.id.rlToCert);
         rlToCert.setOnClickListener(this);
+        tvNick = (TextView) findViewById(R.id.tvNick);
+        tvTel = (TextView) findViewById(R.id.tvTel);
+        tvSex = (TextView) findViewById(R.id.tvSex);
+        tvAddr = (TextView) findViewById(R.id.tvAddr);
+        tvCert = (TextView) findViewById(R.id.tvCertType);
     }
 
     @Override
@@ -47,5 +55,18 @@ public class IndividualActivity extends BaseActivity implements View.OnClickList
                 break;
             default:break;
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadUserInfo();
+    }
+
+    private void loadUserInfo(){
+        CfUser cfUser = MyApplication.getCfUser();
+        tvNick.setText(cfUser.user.nick);
+        tvTel.setText(cfUser.user.tel);
+        //tvSex.setText(cfUser.user.sex);
     }
 }
